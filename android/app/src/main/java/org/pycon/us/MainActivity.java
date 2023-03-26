1473d824dc1ebeba477dcfade31350880726e647
Ee Durbin <ewdurbin@gmail.com>
2023-03-26 06:40:13 -0400

    apply our branding

---
+++ android/app/src/main/java/org/pycon/us/MainActivity.java
@@ -0,0 +1,5 @@
+package org.pycon.us;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,25 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'org.pycon.us.2023',
-  appName: 'pycon-us-2023-onsite-app',
+  appId: 'org.pycon.us',
+  appName: 'PyCon US 2023',
   webDir: 'www',
   bundledWebRuntime: false,
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 3000,
-      launchAutoHide: true,
-      backgroundColor: "#ffffffff",
-      androidSplashResourceName: "splash",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: true,
-      androidSpinnerStyle: "large",
-      iosSpinnerStyle: "small",
-      spinnerColor: "#999999",
-      splashFullScreen: true,
-      splashImmersive: true,
-    },
-  },
 };
 
 export default config;
