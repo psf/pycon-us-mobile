eb653927c3d571cca4a8a5810d32eadbad429497
Ee Durbin <ewdurbin@gmail.com>
2023-04-01 11:03:25 -0400

    infinite scroll for speakers

--- src/app/pages/speaker-list/speaker-list.ts
+++ src/app/pages/speaker-list/speaker-list.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { ConferenceData } from '../../providers/conference-data';
-import { Config } from '@ionic/angular';
+import { Config, InfiniteScrollCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'page-speaker-list',
@@ -22,24 +22,41 @@ export class SpeakerListPage implements OnInit {
   ) {}
 
   updateSpeakers() {
-    this.confData.getSpeakers(this.speakerQueryText).subscribe((speakers: any[]) => {
+    this.confData.getSpeakers("").subscribe((speakers: any[]) => {
       this.speakers = speakers;
       this.generateSpeakers();
     });
   }
 
-  private generateSpeakers() {
+  searchSpeakers() {
+    this.displaySpeakers = [];
+    this.confData.getSpeakers(this.speakerQueryText).subscribe((speakers: any[]) => {
+      this.displaySpeakers = speakers;
+    });
+  }
+
+  resetSpeakers() {
+    this.displaySpeakers = [];
+    this.changeDetection.detectChanges();
+    this.updateSpeakers();
+  }
+
+  async generateSpeakers() {
     if (this.page >= 0) {
-      console.log(this.speakers);
-      console.log(this.page, 25*this.page, 25*this.page + 24);
-      this.speakers.slice(25*this.page, 25*this.page + 24).forEach(speaker => this.displaySpeakers.push(speaker));
+      this.speakers.slice(25*this.page, 25*this.page + 25).forEach(speaker => this.displaySpeakers.push(speaker));
+      console.log(this.speakers.length, this.displaySpeakers.length)
       if (this.speakers.length == this.displaySpeakers.length) {
         this.page = -1
       } else {
         this.page += 1;
+        this.changeDetection.detectChanges();
       }
-      this.changeDetection.detectChanges();
     }
+  }
+
+  onIonInfinite(ev) {
+    this.generateSpeakers();
+    (ev as InfiniteScrollCustomEvent).target.complete();
   }
 
   handleRefresh(event) {
