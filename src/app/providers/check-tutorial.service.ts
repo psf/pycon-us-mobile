6836c2d9f0d94d0de4fb9c04f79282559402c248
Ee Durbin <ewdurbin@gmail.com>
2023-04-02 12:09:48 -0400

    redirect people with lead retrieval there first, since its likely 99.9999% of the reason they are opening the app

--- src/app/providers/check-tutorial.service.ts
+++ src/app/providers/check-tutorial.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { CanLoad, Router } from '@angular/router';
 import { Storage } from '@ionic/storage';
+import { UserData } from './user-data';
+
 @Injectable({
   providedIn: 'root'
 })
 export class CheckTutorial implements CanLoad {
-  constructor(private storage: Storage, private router: Router) {}
+  constructor(private storage: Storage, private router: Router, private userData: UserData) {}
 
   canLoad() {
     return this.storage.get('ion_did_tutorial').then(res => {
+      this.userData.checkHasLeadRetrieval().then(hasLead => {
+        if (hasLead) {
+          this.router.navigate(['/app', 'tabs', 'lead-retrieval']);
+          return false;
+        }
+      })
       if (res) {
         this.router.navigate(['/app', 'tabs', 'schedule']);
         return false;
