a4b71a6d50ae70f4b2e05ee908989ecd7dc7dae1
Ee Durbin <ewdurbin@gmail.com>
2023-04-02 10:15:43 -0400

    we do not accept signups via the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -94,10 +94,6 @@ export class AppComponent implements OnInit {
       this.updateLoggedInStatus(true);
     });
 
-    window.addEventListener('user:signup', () => {
-      this.updateLoggedInStatus(true);
-    });
-
     window.addEventListener('user:logout', () => {
       this.updateLoggedInStatus(false);
     });
