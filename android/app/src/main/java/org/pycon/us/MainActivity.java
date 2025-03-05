48a354096a39000238c9ccd64d95ae3eb6180235
Ee Durbin <ewdurbin@gmail.com>
2025-03-05 07:15:09 -0500

    android: fix rendering of UI on first load
    ref: https://github.com/ionic-team/capacitor/issues/6423#issuecomment-2654322195

--- android/app/src/main/java/org/pycon/us/MainActivity.java
+++ android/app/src/main/java/org/pycon/us/MainActivity.java
@@ -1,5 +1,15 @@
 package org.pycon.us;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		getWindow().getDecorView().setSystemUiVisibility(
+			android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+		);
+	}
+}
