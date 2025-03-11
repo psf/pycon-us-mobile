5c3dbff8fbd4603715b2c224198b537f2f1368ff
Ee Durbin <ewdurbin@gmail.com>
2025-03-11 16:46:25 -0400

    Implement a way for logged-in users to share their mobile app state with us.
    Encrypts the user's localstorage using their api auth secret. This will be useful for debugging if (when) folks have issues with the app.
    
    Notably, this should allow us to save scans for lead retrieval, door check, and redemption if something goes awry.

--- src/app/pages/about-pycon/about-pycon.page.ts
+++ src/app/pages/about-pycon/about-pycon.page.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
+import { Directory, Filesystem } from '@capacitor/filesystem';
+import { Storage } from '@ionic/storage-angular';
+import { Share } from '@capacitor/share';
+import { encode } from 'js-base64';
+import * as crypto from 'crypto-js';
 
+import { UserData } from '../../providers/user-data';
 import { ConferenceData } from '../../providers/conference-data';
 import { LiveUpdateService } from '../../providers/live-update.service';
 
@@ -11,11 +17,14 @@ import { LiveUpdateService } from '../../providers/live-update.service';
 })
 export class AboutPyconPage implements OnInit {
   content: any = "";
+  loggedIn: boolean = false;
 
   constructor(
     private loadingCtrl: LoadingController,
     private confData: ConferenceData,
+    private userData: UserData,
     private changeDetection: ChangeDetectorRef,
+    private storage: Storage,
     public liveUpdateService: LiveUpdateService,
   ) {}
 
@@ -30,6 +39,42 @@ export class AboutPyconPage implements OnInit {
         this.changeDetection.detectChanges();
         setTimeout(() => {loader.dismiss()}, 100);
       });
+    });
+    this.userData.isLoggedIn().then((resp) => {
+      this.loggedIn = resp;
+    });
+  }
+
+  async shareDebug() {
+    let kvstore = []
+    const email = await this.storage.get("email");
+    const key = await this.storage.get("key");
+    const secret = await this.storage.get("secret");
+    await this.storage.forEach((value, key, index) => {
+      kvstore.push([index, key, value]);
+    });
+    const dbdump = JSON.stringify(kvstore);
+    const data = {
+        email: email,
+        key: key,
+        state: encode(crypto.AES.encrypt(dbdump, secret).toString()),
+    }
+    const base64Data = encode(JSON.stringify(data));
+    return Filesystem.writeFile({
+      path: "pycon-db-dump-" + key + ".dat",
+      data: base64Data,
+      directory: Directory.Cache
+    }).then(() => {
+      return Filesystem.getUri({
+        directory: Directory.Cache,
+        path: 'pycon-db-dump-' + key + '.dat',
+      });
+    }).then((uriResult) => {
+      return Share.share({
+        title: "Share Debug Records with PyCon Staff",
+        text: 'pycon-db-dump.dat',
+        url: uriResult.uri,
+      })
     });
   }
 
